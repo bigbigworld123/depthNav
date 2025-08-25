ea5a9d3d847b883f5e0f8141cac605446b118363
jonlee48 <jonathan.lee48@gmail.com>
2025-08-25 12:21:00 -0400

    deleted unnecessary files

--- examples/navigation/train_bptt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python depthnav/scripts/train_bptt.py \
-    --logging_root examples/navigation/logs/debug \
-    --cfg_file examples/navigation/cfg/nav_baseline.yaml \
-    --run_name debugging \
-    --render
